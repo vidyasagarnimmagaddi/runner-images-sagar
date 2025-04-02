f67c027a0f946e3d02b9425657000a0b29a52d9c
vidyasagarnimmagaddi <vidyasagarnimmagaddi@github.com>
2025-04-02 16:57:47 +0530

    [Ubuntu] Adding PHP to latest-version:8.4

--- images/ubuntu/scripts/build/install-php.sh
+++ images/ubuntu/scripts/build/install-php.sh
@@ -77,7 +77,7 @@ for version in $php_versions; do
         apt-get install --no-install-recommends php$version-recode
     fi
 
-    if [[ $version != "8.0" && $version != "8.1" && $version != "8.2" && $version != "8.3" ]]; then
+    if [[ $version != "8.0" && $version != "8.1" && $version != "8.2" && $version != "8.3" && $version != "8.4" ]]; then
         apt-get install --no-install-recommends php$version-xmlrpc php$version-json
     fi
 done
