a8f47b6895f6b292e0eb7ae85ae4931e4a7f35f9
vidyasagarnimmagaddi <vidyasagarnimmagaddi@github.com>
2025-06-30 12:10:00 +0530

    kotlin_tests

--- images/windows/scripts/tests/Tools.Tests.ps1
+++ images/windows/scripts/tests/Tools.Tests.ps1
@@ -201,15 +201,21 @@ Describe "Pipx" {
 }
 
 Describe "Kotlin" {
-    $kotlinPackages = @("kapt", "kotlin", "kotlinc", "kotlinc-js", "kotlinc-jvm")
+    $kotlinPackages = @("kotlin", "kotlinc", "kotlinc-js", "kotlinc-jvm", "kapt") 
 
     It "<toolName> is available" -TestCases ($kotlinPackages | ForEach-Object { @{ toolName = $_ } }) {
-        switch ($toolName) {
-            "kapt"        { $command = "kapt -Kapt-mode=stubsAndApt -version" }
-            "kotlinc-js"  { $command = "kotlinc-js -help" }
-            default       { $command = "$toolName -version" }
+        if ($toolName -eq "kotlinc-js") {
+            $cmd = "kotlinc-js -version -Xir-module-name=test -ir-output-name=test.js -ir-output-dir=."
+            $cmd | Should -ReturnZeroExitCode
         }
-        $command | Should -ReturnZeroExitCode
+        elseif ($toolName -eq "kapt") {
+            # Checks if the kapt executable is available in the PATH
+            (Get-Command "kapt" -ErrorAction SilentlyContinue) | Should -Not -BeNullOrEmpty
+        }
+        else {
+            $cmd = "$toolName -version"
+            $cmd | Should -ReturnZeroExitCode
+        }
     }
 }
 
